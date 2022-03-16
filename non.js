2e55141bd98ec02c7a680753c0543743102426d3
S3-PHB-01NegeriLaskarPelangi <michaelxips3@gmail.com>
2022-03-16 07:11:18 +0700

    update

---
+++ non.js
@@ -0,0 +1,20 @@
+var lagi = true
+
+   
+
+     while (lagi === true) {
+  var nama = prompt('Tak kenal maka tak sayang\n(Isi nama kamu terlebih dahulu)');
+  alert('Halo ' +nama +' salam kenal');
+  alert('Setelah ini kamu akan mengakses Mading Kelompok 1 PHB XIPS3 '+ nama);
+  alert('Penasaran kan ' +nama?);
+  alert('Oke, daripada berlama-lama silahkan cari tahu sendiri :)')
+  alert('Untuk full screen tinggal tekan f11');
+  alert('Ok itu aja informasinya, selebihnya terimakasih '+ nama);
+  lagi = confirm('Mau baca informasinya lagi?');
+
+}
+alert('Enjoy this web ' + nama);
+alert('Dan semoga setelah membaca web ini, wawasan kalian semakin bertambah :)');
+
+    
+    
---
+++ peringatan.js
@@ -0,0 +1,19 @@
+var lagi = true
+
+   
+
+     while (lagi === true) {
+  var nama = prompt('Tak kenal maka tak sayang\n(Isi nama kamu terlebih dahulu)');
+  alert('Halo ' +nama +' salam kenal🖐️');
+  alert('Setelah ini kamu akan mengakses Mading Kelompok 1 PHB XIPS3 '+ nama);
+  alert('Penasaran kan ' +nama + "?🤔" );
+
+  alert('Terimakasih udah mau mampir ke website kami '+ nama + "😄");
+  lagi = confirm('Mau baca informasinya lagi?');
+
+}
+alert('Enjoy this web ' + nama +"😉");
+alert('Dan semoga setelah membaca web ini, wawasanmu semakin bertambah😎');
+
+    
+    
